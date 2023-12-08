cef89e6becf1f1528707ec70162776bf7d186dfa
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2023-12-07 21:59:13 -0500

    yes

--- pruners/PTMGP.py
+++ pruners/PTMGP.py
@@ -7,12 +7,10 @@ def set_PMGP(model, train_size, max_train_steps, args):
     return 0
 
 
-
-
 class PMGP(object):
     
     def __init__(self, model, train_size, max_train_steps,
-                 final_ratio, initial_ratio=1,
+                 final_ratio=1, initial_ratio=1,
                  sigma0=1e-12, sigma1=0.1, lambda_mix=1e-7,
                  anneal_start = 0, anneal_end = 0,
                  initial_warmup = 0.0, final_warmup = 1, warmup_steps = 1000,
@@ -29,6 +27,7 @@ class PMGP(object):
 
         self.anneal_start = anneal_start
         self.anneal_end = anneal_end
+        self.prior_warmup_steps = anneal_end - anneal_start
 
         self.final_ratio = final_ratio
         self.initial_ratio = initial_ratio
@@ -47,7 +46,7 @@ class PMGP(object):
             print ("max_train_steps:", max_train_steps)
             raise ValueError("Wrong schedule values!")
     
-        self.spars_schedu_steps = cubic_prune_end - cubic_prune_start
+        self.ratio_scheduler_steps = cubic_prune_end - cubic_prune_start
         self.cubic_prune_start = cubic_prune_start
         self.cubic_prune_end = cubic_prune_end
 
@@ -68,7 +67,7 @@ class PMGP(object):
     
     def add_prior_grad(self, train_step_index):
         
-        anneal_start = self.prior_start
+        anneal_start = self.anneal_start
         anneal_end = self.anneal_end
         prior_warmup_steps = self.prior_warmup_steps
         
@@ -79,9 +78,9 @@ class PMGP(object):
         else:
             anneal_lambda = 1.0
                 
-        lambda_n = self.config["lambdan"]
-        prior_sigma_1 = self.config["sigma1"]
-        prior_sigma_0 = self.config["sigma0"]
+        lambda_mix = self.lambda_mix
+        sigma_1 = self.sigma1
+        sigma_0 = self.sigma0
         c1, c2, prior_threshold = self.calculate_prior_threshold()
         
         if anneal_lambda > 0:
@@ -89,61 +88,59 @@ class PMGP(object):
                 for pname, param in self.model.named_parameters():
                     if self.non_mask_name == None or not any(nd in pname for nd in self.non_mask_name):
                         temp = param.pow(2).mul(c2).add(c1).exp().add(1).pow(-1)
-                        temp = param.div(-prior_sigma_0).mul(temp) + param.div(-prior_sigma_1).mul(1 - temp)
+                        temp = param.div(-sigma_0).mul(temp) + param.div(-sigma_1).mul(1 - temp)
                         prior_grad = temp.div(self.train_size)
                         param.grad.data -= anneal_lambda*prior_grad
-                    
         return prior_threshold
     
-    # some functions below are adopted and modified based on https://github.com/QingruZhang/PLATON/blob/main/Pruner.py
-    def mask_with_threshold(self, model, threshold):
+    def mask_with_threshold(self, ratio):
         
-        # Calculate the final importance score
+        # Calculate the importance score
         is_dict = {}
-        for n, p in model.named_parameters():
+        for n, p in self.model.named_parameters():
             if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
                 is_dict[n] = p.abs().detach()
                     
         # Calculate the mask threshold
         all_is = torch.cat([is_dict[n].view(-1) for n in is_dict])
-        mask_threshold = torch.kthvalue(all_is, int(all_is.shape[0] * (1 - threshold)))[0].item()
+        mask_threshold = torch.kthvalue(all_is, int(all_is.shape[0] * (1 - ratio)))[0].item()
         
         # Mask weights whose importance lower than threshold
-        for n, p in model.named_parameters():
+        for n, p in self.model.named_parameters():
             if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
                 p.data.masked_fill_(is_dict[n] < mask_threshold, 0.0)
                 
         return mask_threshold
    
-    def magnitude_pruning(self, model, train_step_index):
+    def magnitude_pruning(self, train_step_index):
         # Get the remaining ratio
-        threshold, mask_ind = self.schedule_threshold_comb(train_step_index)
+        ratio, mask_ind = self.cubic_remaining_ratio_scheduler(train_step_index)
         if mask_ind:
             # Mask weights during masking horizon
-            mask_threshold = self.mask_with_threshold(model, threshold)
+            mask_threshold = self.mask_with_threshold(ratio)
         else:
             mask_threshold = None
         
-        return threshold, mask_threshold
+        return ratio, mask_threshold
     
-    def schedule_threshold_comb(self, step):
+    def cubic_remaining_ratio_scheduler(self, train_step_index):
         
         # Schedule the remaining ratio
-        initial_threshold = self.config['initial_threshold']
-        final_threshold = self.config['final_threshold']
-        deltaT = self.config['deltaT']
-        cubic_prune_start = self.config["cubic_prune_start"]
-        cubic_prune_end = self.config["cubic_prune_end"]
-        spars_schedu_steps = self.config["spars_schedu_steps"]
+        initial_ratio = self.initial_ratio
+        final_ratio = self.final_ratio
+        deltaT = self.deltaT
+        cubic_prune_start = self.cubic_prune_start
+        cubic_prune_end = self.cubic_prune_end
+        ratio_scheduler_steps = self.ratio_scheduler_steps
         mask_ind = False
-        if step <= cubic_prune_start:
-            threshold = initial_threshold
+        if train_step_index <= cubic_prune_start:
+            threshold = initial_ratio
             mask_ind = False
-        elif step > cubic_prune_end:
-            threshold = final_threshold
+        elif train_step_index > cubic_prune_end:
+            threshold = final_ratio
             mask_ind = True
         else:
-            mul_coeff = 1 - (step - cubic_prune_start) / (spars_schedu_steps)
-            threshold = final_threshold + (initial_threshold - final_threshold) * (mul_coeff ** 3)
-            mask_ind = True if step % deltaT == 0 else False
+            mul_coeff = 1 - (train_step_index - cubic_prune_start) / (ratio_scheduler_steps)
+            threshold = final_ratio + (initial_ratio - final_ratio) * (mul_coeff ** 3)
+            mask_ind = True if train_step_index % deltaT == 0 else False
         return threshold, mask_ind
