478048446acb465662bbf015bc04ac4d40af645c
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2023-12-24 15:32:41 -0500

    add the algorithm

--- GLUE/train.py
+++ GLUE/train.py
@@ -18,6 +18,7 @@ from transformers import (
 )
 
 import composer
+from composer import Time, TimeUnit
 from composer.utils import dist
 from composer.models.huggingface import HuggingFaceModel
 from torchmetrics.classification import MulticlassAccuracy, MulticlassMatthewsCorrCoef, MulticlassF1Score
@@ -25,6 +26,7 @@ from torchmetrics.regression import SpearmanCorrCoef, PearsonCorrCoef
 from composer import Trainer
 from composer.callbacks import LRMonitor, RuntimeEstimator
 from composer.loggers import WandBLogger
+from pruners.PMGP import set_PMGP_Algorithm
 
 
 task_to_keys = {
@@ -133,10 +135,25 @@ def parse_args():
     # reproducibility
     parser.add_argument("--seed", type=int, default=42, help="Random seed to use for reproducibility.")
 
+    # PMGP
+    parser.add_argument("--final_ratio", type=float, default=0.1, help="The final ratio of the remaining weights.")
+    parser.add_argument("--initial_ratio", type=float, default=1, help="The initial ratio of the remaining weights.")
+
+    parser.add_argument("--sigma0", type=float, default=1e-12, help="The smaller variance of the Mixture Gaussian prior.")
+    parser.add_argument("--sigma1", type=float, default=0.1, help="The larger variance of the Mixture Gaussian orior.")
+    parser.add_argument("--lambda_mix", type=float, default=1e-7, help="The mixing coefficient of the Mixture Gaussian prior.")
+    parser.add_argument("--anneal_start", type=int, default=0, help="The number of traing batches/steps for annealing to start.")
+    parser.add_argument("--anneal_end", type=int, default="1ep", help="The number of traing batches/steps for annealing to end.")
+
+    parser.add_argument("--initial_warmup", type=int, default=1, help="The number of training batches/steps for initial warmup.")
+    parser.add_argument("--final_warmup", type=int, default=1, help="The number of training batches/steps for final warmup.")
+    parser.add_argument("--warmup_steps", type=int, default=100, help="The number of warmup steps.")
+    parser.add_argument("--deltaT", type=int, default=10, help="The interval to mask weights.")
+
+    parser.add_argument('--non_mask_name', nargs='+', type=str, default=["layernorm", "classifier", "pooler"])
+
     args = parser.parse_args()
     return args
-
-
 
 
 def main():
@@ -251,6 +268,17 @@ def main():
         name=args.wandb_name
     )
 
+    # initialize the PMGP algorithm
+    train_size = len(train_dataset)
+    train_time = Time.from_timestring(args.max_duration)
+    if train_time.unit == TimeUnit.EPOCH:
+        max_train_steps = len(train_dataloader) * train_time.value
+    elif train_time.unit == TimeUnit.BATCH:
+        max_train_steps = train_time.value
+    else:
+        raise ValueError(f"Unsupported time unit: {train_time.unit}")
+    pruner_algorithm = set_PMGP_Algorithm(train_size, max_train_steps, args)
+
     # initialize the trainer
     trainer = Trainer(
 
@@ -275,7 +303,7 @@ def main():
         callbacks=[LRMonitor(), RuntimeEstimator()],
 
         # algorithms
-        algorithms=None,
+        algorithms=[pruner_algorithm],
 
         # checkpointing
         run_name=args.run_name,
--- pruners/PMGP.py
+++ pruners/PMGP.py
@@ -1,4 +1,5 @@
 import torch
+import re
 import numpy as np
 from composer.core import Algoritm, Event
 
@@ -14,7 +15,7 @@ class PMGP_Algorithm(Algorithm):
         
         self.train_size = train_size
         self.max_train_steps = max_train_steps
-        self.non_mask_name = non_mask_name
+        self.non_mask_name_pattern = re.compile("|".join(non_mask_name), re.IGNORECASE) if non_mask_name is not None else None
 
         self.lambda_mix = lambda_mix
         self.sigma0 = sigma0
@@ -46,6 +47,12 @@ class PMGP_Algorithm(Algorithm):
         self.cubic_prune_start = cubic_prune_start
         self.cubic_prune_end = cubic_prune_end
 
+    def whether_mask_para(self, n):
+        if self.non_mask_name_pattern == None:
+            return True
+        else:
+            return bool(re.search(self.non_mask_name_pattern, n))
+
     def calculate_prior_threshold(self):
 
         lambda_mix = self.lambda_mix
@@ -60,7 +67,7 @@ class PMGP_Algorithm(Algorithm):
         
         return c1, c2, prior_threshold
     
-    def add_prior_grad(self, model, train_step_index):
+    def add_prior_grad(self, model, train_step_index, device="cuda"):
         
         anneal_start = self.anneal_start
         anneal_end = self.anneal_end
@@ -79,12 +86,13 @@ class PMGP_Algorithm(Algorithm):
         
         if anneal_lambda > 0:
             with torch.no_grad():
-                for pname, param in model.named_parameters():
-                    if self.non_mask_name == None or not any(nd in pname for nd in self.non_mask_name):
-                        temp = param.pow(2).mul(c2).add(c1).exp().add(1).pow(-1)
-                        temp = param.div(-sigma_0).mul(temp) + param.div(-sigma_1).mul(1 - temp)
-                        prior_grad = temp.div(self.train_size)
-                        param.grad.data -= anneal_lambda*prior_grad
+                for n, p in model.named_parameters():
+                    if self.whether_mask_para(n):
+                        with torch.autocast(device_type=device):
+                            temp = p.pow(2).mul(c2).add(c1).exp().add(1).pow(-1)
+                            temp = p.div(-sigma_0).mul(temp) + p.div(-sigma_1).mul(1 - temp)
+                            prior_grad = temp.div(self.train_size)
+                            p.grad.data -= anneal_lambda*prior_grad
         return prior_threshold
     
     def mask_with_threshold(self, model, ratio):
@@ -92,7 +100,7 @@ class PMGP_Algorithm(Algorithm):
         # Calculate the importance score
         is_dict = {}
         for n, p in model.named_parameters():
-            if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
+            if self.whether_mask_para(n):
                 is_dict[n] = p.abs().detach()
                     
         # Calculate the mask threshold
@@ -101,7 +109,7 @@ class PMGP_Algorithm(Algorithm):
         
         # Mask weights whose importance lower than threshold
         for n, p in model.named_parameters():
-            if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
+            if self.whether_mask_para(n):
                 p.data.masked_fill_(is_dict[n] < mask_threshold, 0.0)
                 
         return mask_threshold
@@ -129,25 +137,30 @@ class PMGP_Algorithm(Algorithm):
         ratio_scheduler_steps = self.ratio_scheduler_steps
         mask_ind = False
         if train_step_index <= cubic_prune_start:
-            threshold = initial_ratio
+            ratio = initial_ratio
             mask_ind = False
         elif train_step_index > cubic_prune_end:
-            threshold = final_ratio
+            ratio = final_ratio
             mask_ind = True
         else:
             mul_coeff = 1 - (train_step_index - cubic_prune_start) / (ratio_scheduler_steps)
-            threshold = final_ratio + (initial_ratio - final_ratio) * (mul_coeff ** 3)
+            ratio = final_ratio + (initial_ratio - final_ratio) * (mul_coeff ** 3)
             mask_ind = True if train_step_index % deltaT == 0 else False
-        return threshold, mask_ind
+        return ratio, mask_ind
 
     def match(self, event, state):
         return event in [Event.AFTER_TRAIN_BATCH,  Event.BATCH_END]
 
     def apply(self, event, state, logger):
         if event == Event.AFTER_TRAIN_BATCH:
-            self.add_prior_grad(state.model, state.timestamp)
+            prior_threshold = self.add_prior_grad(state.model, state.timestamp.batch, state.device)
+            logger.log_metrics({"prior_threshold": float(prior_threshold)})
         elif event == Event.BATCH_END:
-            self.magnitude_pruning(state.model, state.timestamp)
+            ratio, mask_threshold = self.magnitude_pruning(state.model, state.timestamp.batch)
+            logger.log_metrics({"remaining_ratio": float(ratio)})
+            if mask_threshold is None:
+                mask_threshold = 0.0
+            logger.log_metrics({"mask_threshold": float(mask_threshold)})
 
 def set_PMGP_Algorithm(train_size, max_train_steps, args):
     pruner_algorithm = PMGP_Algorithm(
@@ -160,144 +173,3 @@ def set_PMGP_Algorithm(train_size, max_train_steps, args):
                     non_mask_name=args.non_mask_name
                     )
     return pruner_algorithm
-
-
-class PMGP(object):
-    
-    def __init__(self, model, train_size, max_train_steps,
-                 final_ratio=1, initial_ratio=1,
-                 sigma0=1e-12, sigma1=0.1, lambda_mix=1e-7,
-                 anneal_start = 0, anneal_end = 0,
-                 initial_warmup = 0.0, final_warmup = 1, warmup_steps = 1000, deltaT = 1,
-                 non_mask_name=None):
-        
-        self.train_size = train_size
-        self.model = model
-        self.max_train_steps = max_train_steps
-        self.non_mask_name = non_mask_name
-
-        self.lambda_mix = lambda_mix
-        self.sigma0 = sigma0
-        self.sigma1 = sigma1
-
-        self.anneal_start = anneal_start
-        self.anneal_end = anneal_end
-        self.prior_warmup_steps = anneal_end - anneal_start
-
-        self.final_ratio = final_ratio
-        self.initial_ratio = initial_ratio
-        
-        self.initial_warmup = initial_warmup
-        self.final_warmup = final_warmup
-        self.warmup_steps = warmup_steps
-        self.deltaT = deltaT
-        
-        cubic_prune_start = anneal_end + initial_warmup*warmup_steps
-        cubic_prune_end = max_train_steps - final_warmup*warmup_steps
-        
-        if not (anneal_end <= cubic_prune_start <= cubic_prune_end <= max_train_steps):
-            print ("anneal_end:", anneal_end)
-            print ("cubic_prune_start:", cubic_prune_start)
-            print ("cubic_prune_end:", cubic_prune_end)
-            print ("max_train_steps:", max_train_steps)
-            raise ValueError("Wrong schedule values!")
-    
-        self.ratio_scheduler_steps = cubic_prune_end - cubic_prune_start
-        self.cubic_prune_start = cubic_prune_start
-        self.cubic_prune_end = cubic_prune_end
-
-    
-    def calculate_prior_threshold(self):
-
-        lambda_mix = self.lambda_mix
-        sigma_1 = self.sigma1
-        sigma_0 = self.sigma0
-
-
-        c1 = np.log(lambda_mix) - np.log(1 - lambda_mix) + 0.5 * np.log(sigma_0) - 0.5 * np.log(sigma_1)
-        c2 = 0.5 / sigma_0 - 0.5 / sigma_1
-        prior_threshold = np.sqrt(np.log((1 - lambda_mix) / lambda_mix * np.sqrt(sigma_1 / sigma_0)) / (
-                    0.5 / sigma_0 - 0.5 / sigma_1))
-        
-        return c1, c2, prior_threshold
-    
-    def add_prior_grad(self, train_step_index):
-        
-        anneal_start = self.anneal_start
-        anneal_end = self.anneal_end
-        prior_warmup_steps = self.prior_warmup_steps
-        
-        if train_step_index < anneal_start:
-            anneal_lambda = 0
-        elif anneal_start <= train_step_index < anneal_end:
-            anneal_lambda = 1.0 * (train_step_index - anneal_start)/prior_warmup_steps
-        else:
-            anneal_lambda = 1.0
-                
-        lambda_mix = self.lambda_mix
-        sigma_1 = self.sigma1
-        sigma_0 = self.sigma0
-        c1, c2, prior_threshold = self.calculate_prior_threshold()
-        
-        if anneal_lambda > 0:
-            with torch.no_grad():
-                for pname, param in self.model.named_parameters():
-                    if self.non_mask_name == None or not any(nd in pname for nd in self.non_mask_name):
-                        temp = param.pow(2).mul(c2).add(c1).exp().add(1).pow(-1)
-                        temp = param.div(-sigma_0).mul(temp) + param.div(-sigma_1).mul(1 - temp)
-                        prior_grad = temp.div(self.train_size)
-                        param.grad.data -= anneal_lambda*prior_grad
-        return prior_threshold
-    
-    def mask_with_threshold(self, ratio):
-        
-        # Calculate the importance score
-        is_dict = {}
-        for n, p in self.model.named_parameters():
-            if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
-                is_dict[n] = p.abs().detach()
-                    
-        # Calculate the mask threshold
-        all_is = torch.cat([is_dict[n].view(-1) for n in is_dict])
-        mask_threshold = torch.kthvalue(all_is, int(all_is.shape[0] * (1 - ratio)))[0].item()
-        
-        # Mask weights whose importance lower than threshold
-        for n, p in self.model.named_parameters():
-            if self.non_mask_name == None or not any(nd in n for nd in self.non_mask_name):
-                p.data.masked_fill_(is_dict[n] < mask_threshold, 0.0)
-                
-        return mask_threshold
-   
-    def magnitude_pruning(self, train_step_index):
-        
-        # Get the remaining ratio
-        ratio, mask_ind = self.cubic_remaining_ratio_scheduler(train_step_index)
-        if mask_ind:
-            # Mask weights during masking horizon
-            mask_threshold = self.mask_with_threshold(ratio)
-        else:
-            mask_threshold = None
-        
-        return ratio, mask_threshold
-    
-    def cubic_remaining_ratio_scheduler(self, train_step_index):
-        
-        # Schedule the remaining ratio
-        initial_ratio = self.initial_ratio
-        final_ratio = self.final_ratio
-        deltaT = self.deltaT
-        cubic_prune_start = self.cubic_prune_start
-        cubic_prune_end = self.cubic_prune_end
-        ratio_scheduler_steps = self.ratio_scheduler_steps
-        mask_ind = False
-        if train_step_index <= cubic_prune_start:
-            threshold = initial_ratio
-            mask_ind = False
-        elif train_step_index > cubic_prune_end:
-            threshold = final_ratio
-            mask_ind = True
-        else:
-            mul_coeff = 1 - (train_step_index - cubic_prune_start) / (ratio_scheduler_steps)
-            threshold = final_ratio + (initial_ratio - final_ratio) * (mul_coeff ** 3)
-            mask_ind = True if train_step_index % deltaT == 0 else False
-        return threshold, mask_ind
