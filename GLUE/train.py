7aca999d5dffcc0e15cab91039aa0304619e5681
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2024-01-21 16:35:18 -0500

    same thing as last commit

--- GLUE/train.py
+++ GLUE/train.py
@@ -279,21 +279,27 @@ def parse_args():
     parser.add_argument("--final_ratio",        type=float, default=0.2, help="The final ratio of the remaining weights.")
     parser.add_argument("--initial_ratio",      type=float, default=1,   help="The initial ratio of the remaining weights.")
     parser.add_argument("--initial_warmup",     type=my_custom_type,   default=1,   help="The number of training batches/steps for initial warmup.")
-    parser.add_argument("--final_warmup",       type=my_custom_type,   default=1,   help="The number of training batches/steps for final warmup.")
+    parser.add_argument("--final_warmup",       type=my_custom_type,   default=0,   help="The number of training batches/steps for final warmup.")
     parser.add_argument("--deltaT",             type=my_custom_type,   default=10,  help="The interval to mask weights.")
+    parser.add_argument("--sparse_fine_tune",   type=my_custom_type,   default=0,   help="The number of training batches/steps for sparse fine-tuning.")
 
-    # PMGP
+    # BReg and PMGP
     parser.add_argument("--sigma0",             type=float, default=1e-15, help="The smaller variance of the Mixture Gaussian prior.")
-    parser.add_argument("--sigma1",             type=float, default=0.1,   help="The larger variance of the Mixture Gaussian prior.")
+    parser.add_argument("--alpha_i_sigma0",     type=float, default=1.0,   help="The initial factor value of the sigma0.")
+    parser.add_argument("--alpha_f_sigma0",     type=float, default=1.0,  help="The final factor value of the sigma0.")
 
+    parser.add_argument("--sigma1",             type=float, default=0.1,   help="The larger variance of the Mixture Gaussian prior.")
+    parser.add_argument("--alpha_i_sigma1",     type=float, default=1.0,   help="The initial factor value of the sigma1.")
+    parser.add_argument("--alpha_f_sigma1",     type=float, default=1.0,   help="The final factor value of the sigma1.")
+    
     parser.add_argument("--lambda_mix",         type=float, default=1e-3,  help="The mixing coefficient of the Mixture Gaussian prior.")
     parser.add_argument("--alpha_i_lambda",     type=float, default=1.0,   help="The initial factor value of the lambda_mix.")
     parser.add_argument("--alpha_f_lambda",     type=float, default=0.01,  help="The final factor value of the lambda_mix.")
 
-    parser.add_argument("--anneal_start_lambda",type=my_custom_type,   default=None,  help="The number of traing batches/steps for lambda_mix annealing to start.")
-    parser.add_argument("--anneal_end_lambda",  type=my_custom_type,   default=None,  help="The number of traing batches/steps for lambda_mix annealing to end.")
-    parser.add_argument("--masking_value",      type=float, default=0.0,   help="The masking value for the pruned weights.")
-    parser.add_argument('--non_prior_name',     
+    parser.add_argument("--anneal_start",       type=my_custom_type,   default=None,  help="The number of traing batches/steps for lambda_mix annealing to start.")
+    parser.add_argument("--anneal_end",         type=my_custom_type,   default=None,  help="The number of traing batches/steps for lambda_mix annealing to end.")
+    parser.add_argument("--masking_value",      type=float,            default=0.0,   help="The masking value for the pruned weights.")
+    parser.add_argument('--non_prior_name',
                         type=str,
                         default=None,
                         nargs='+',
